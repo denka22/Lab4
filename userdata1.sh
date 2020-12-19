96eaf771fa558c07270ab8ada3f82e28fec63351
denka22 <32452518+denka22@users.noreply.github.com>
2020-12-19 12:00:49 +0200

    Update userdata1.sh

--- userdata1.sh
+++ userdata1.sh
@@ -11,4 +11,4 @@ sudo chmod 2775 /var/www
 find /var/www -type d -exec sudo chmod 2775 {} +
 find /var/www -type f -exec sudo chmod 0664 {} +                
 cd /var/www/html
-echo "<h1> My Site <3  2</h1>" > index.html
+echo "<h1>IDO3 LAB</h1>" > index.html
